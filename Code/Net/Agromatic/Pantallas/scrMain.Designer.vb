f8233f10995f096b8358905e49199b834a81e8cc
Cesar Lopez Cortes <cesarlopezcortes@hotmail.com>
2020-08-25 16:19:32 -0500

    pnlMenu code an functions added

--- Code/Net/Agromatic/Pantallas/scrMain.Designer.vb
+++ Code/Net/Agromatic/Pantallas/scrMain.Designer.vb
@@ -27,8 +27,9 @@ Partial Class scrMain
         Me.DragTop = New Bunifu.Framework.UI.BunifuDragControl(Me.components)
         Me.pnlMenu = New System.Windows.Forms.Panel()
         Me.pnlWrapp = New System.Windows.Forms.Panel()
+        Me.BunifuSeparator2 = New Bunifu.Framework.UI.BunifuSeparator()
         Me.btnRecur = New Bunifu.Framework.UI.BunifuFlatButton()
-        Me.btnSens = New Bunifu.Framework.UI.BunifuFlatButton()
+        Me.btnMetro = New Bunifu.Framework.UI.BunifuFlatButton()
         Me.btnCultivos = New Bunifu.Framework.UI.BunifuFlatButton()
         Me.btnTerr = New Bunifu.Framework.UI.BunifuFlatButton()
         Me.pnlTop = New Bunifu.Framework.UI.BunifuGradientPanel()
@@ -37,7 +38,6 @@ Partial Class scrMain
         Me.btnMax = New Bunifu.Framework.UI.BunifuImageButton()
         Me.btnMin = New Bunifu.Framework.UI.BunifuImageButton()
         Me.BunifuSeparator1 = New Bunifu.Framework.UI.BunifuSeparator()
-        Me.BunifuSeparator2 = New Bunifu.Framework.UI.BunifuSeparator()
         Me.pnlMenu.SuspendLayout()
         Me.pnlTop.SuspendLayout()
         CType(Me.btnClose, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -55,7 +55,7 @@ Partial Class scrMain
         'pnlMenu
         '
         Me.pnlMenu.Controls.Add(Me.btnRecur)
-        Me.pnlMenu.Controls.Add(Me.btnSens)
+        Me.pnlMenu.Controls.Add(Me.btnMetro)
         Me.pnlMenu.Controls.Add(Me.btnCultivos)
         Me.pnlMenu.Controls.Add(Me.btnTerr)
         Me.pnlMenu.Controls.Add(Me.BunifuSeparator2)
@@ -72,6 +72,22 @@ Partial Class scrMain
         Me.pnlWrapp.Name = "pnlWrapp"
         Me.pnlWrapp.Size = New System.Drawing.Size(1024, 661)
         Me.pnlWrapp.TabIndex = 2
+        '
+        'BunifuSeparator2
+        '
+        Me.BunifuSeparator2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BunifuSeparator2.BackColor = System.Drawing.Color.Transparent
+        Me.BunifuSeparator2.ForeColor = System.Drawing.SystemColors.Control
+        Me.BunifuSeparator2.LineColor = System.Drawing.Color.FromArgb(CType(CType(39, Byte), Integer), CType(CType(251, Byte), Integer), CType(CType(107, Byte), Integer))
+        Me.BunifuSeparator2.LineThickness = 1
+        Me.BunifuSeparator2.Location = New System.Drawing.Point(4, 49)
+        Me.BunifuSeparator2.Margin = New System.Windows.Forms.Padding(4, 6, 4, 6)
+        Me.BunifuSeparator2.Name = "BunifuSeparator2"
+        Me.BunifuSeparator2.Size = New System.Drawing.Size(1020, 25)
+        Me.BunifuSeparator2.TabIndex = 4
+        Me.BunifuSeparator2.Transparency = 255
+        Me.BunifuSeparator2.Vertical = False
         '
         'btnRecur
         '
@@ -99,7 +115,7 @@ Partial Class scrMain
         Me.btnRecur.Margin = New System.Windows.Forms.Padding(4, 8, 4, 8)
         Me.btnRecur.Name = "btnRecur"
         Me.btnRecur.Normalcolor = System.Drawing.Color.FromArgb(CType(CType(3, Byte), Integer), CType(CType(109, Byte), Integer), CType(CType(25, Byte), Integer))
-        Me.btnRecur.OnHovercolor = System.Drawing.Color.FromArgb(CType(CType(20, Byte), Integer), CType(CType(204, Byte), Integer), CType(CType(96, Byte), Integer))
+        Me.btnRecur.OnHovercolor = System.Drawing.Color.SeaGreen
         Me.btnRecur.OnHoverTextColor = System.Drawing.Color.White
         Me.btnRecur.selected = False
         Me.btnRecur.Size = New System.Drawing.Size(145, 43)
@@ -109,41 +125,41 @@ Partial Class scrMain
         Me.btnRecur.Textcolor = System.Drawing.Color.White
         Me.btnRecur.TextFont = New System.Drawing.Font("Corbel", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         '
-        'btnSens
+        'btnMetro
         '
-        Me.btnSens.Activecolor = System.Drawing.Color.FromArgb(CType(CType(46, Byte), Integer), CType(CType(139, Byte), Integer), CType(CType(87, Byte), Integer))
-        Me.btnSens.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
-        Me.btnSens.BackColor = System.Drawing.Color.FromArgb(CType(CType(3, Byte), Integer), CType(CType(109, Byte), Integer), CType(CType(25, Byte), Integer))
-        Me.btnSens.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.btnSens.BorderRadius = 7
-        Me.btnSens.ButtonText = "Metrología"
-        Me.btnSens.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.btnSens.DisabledColor = System.Drawing.Color.Gray
-        Me.btnSens.Iconcolor = System.Drawing.Color.Transparent
-        Me.btnSens.Iconimage = Global.Agromatic.My.Resources.Resources.sensor__1_
-        Me.btnSens.Iconimage_right = Nothing
-        Me.btnSens.Iconimage_right_Selected = Nothing
-        Me.btnSens.Iconimage_Selected = Nothing
-        Me.btnSens.IconMarginLeft = 0
-        Me.btnSens.IconMarginRight = 0
-        Me.btnSens.IconRightVisible = True
-        Me.btnSens.IconRightZoom = 0R
-        Me.btnSens.IconVisible = True
-        Me.btnSens.IconZoom = 80.0R
-        Me.btnSens.IsTab = False
-        Me.btnSens.Location = New System.Drawing.Point(270, 12)
-        Me.btnSens.Margin = New System.Windows.Forms.Padding(4, 8, 4, 8)
-        Me.btnSens.Name = "btnSens"
-        Me.btnSens.Normalcolor = System.Drawing.Color.FromArgb(CType(CType(3, Byte), Integer), CType(CType(109, Byte), Integer), CType(CType(25, Byte), Integer))
-        Me.btnSens.OnHovercolor = System.Drawing.Color.FromArgb(CType(CType(20, Byte), Integer), CType(CType(204, Byte), Integer), CType(CType(96, Byte), Integer))
-        Me.btnSens.OnHoverTextColor = System.Drawing.Color.White
-        Me.btnSens.selected = False
-        Me.btnSens.Size = New System.Drawing.Size(145, 43)
-        Me.btnSens.TabIndex = 2
-        Me.btnSens.Text = "Metrología"
-        Me.btnSens.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
-        Me.btnSens.Textcolor = System.Drawing.Color.White
-        Me.btnSens.TextFont = New System.Drawing.Font("Corbel", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMetro.Activecolor = System.Drawing.Color.FromArgb(CType(CType(46, Byte), Integer), CType(CType(139, Byte), Integer), CType(CType(87, Byte), Integer))
+        Me.btnMetro.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.btnMetro.BackColor = System.Drawing.Color.FromArgb(CType(CType(3, Byte), Integer), CType(CType(109, Byte), Integer), CType(CType(25, Byte), Integer))
+        Me.btnMetro.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btnMetro.BorderRadius = 7
+        Me.btnMetro.ButtonText = "Metrología"
+        Me.btnMetro.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnMetro.DisabledColor = System.Drawing.Color.Gray
+        Me.btnMetro.Iconcolor = System.Drawing.Color.Transparent
+        Me.btnMetro.Iconimage = Global.Agromatic.My.Resources.Resources.sensor__1_
+        Me.btnMetro.Iconimage_right = Nothing
+        Me.btnMetro.Iconimage_right_Selected = Nothing
+        Me.btnMetro.Iconimage_Selected = Nothing
+        Me.btnMetro.IconMarginLeft = 0
+        Me.btnMetro.IconMarginRight = 0
+        Me.btnMetro.IconRightVisible = True
+        Me.btnMetro.IconRightZoom = 0R
+        Me.btnMetro.IconVisible = True
+        Me.btnMetro.IconZoom = 80.0R
+        Me.btnMetro.IsTab = False
+        Me.btnMetro.Location = New System.Drawing.Point(270, 12)
+        Me.btnMetro.Margin = New System.Windows.Forms.Padding(4, 8, 4, 8)
+        Me.btnMetro.Name = "btnMetro"
+        Me.btnMetro.Normalcolor = System.Drawing.Color.FromArgb(CType(CType(3, Byte), Integer), CType(CType(109, Byte), Integer), CType(CType(25, Byte), Integer))
+        Me.btnMetro.OnHovercolor = System.Drawing.Color.SeaGreen
+        Me.btnMetro.OnHoverTextColor = System.Drawing.Color.White
+        Me.btnMetro.selected = False
+        Me.btnMetro.Size = New System.Drawing.Size(145, 43)
+        Me.btnMetro.TabIndex = 2
+        Me.btnMetro.Text = "Metrología"
+        Me.btnMetro.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnMetro.Textcolor = System.Drawing.Color.White
+        Me.btnMetro.TextFont = New System.Drawing.Font("Corbel", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         '
         'btnCultivos
         '
@@ -171,7 +187,7 @@ Partial Class scrMain
         Me.btnCultivos.Margin = New System.Windows.Forms.Padding(4, 6, 4, 6)
         Me.btnCultivos.Name = "btnCultivos"
         Me.btnCultivos.Normalcolor = System.Drawing.Color.FromArgb(CType(CType(3, Byte), Integer), CType(CType(109, Byte), Integer), CType(CType(25, Byte), Integer))
-        Me.btnCultivos.OnHovercolor = System.Drawing.Color.FromArgb(CType(CType(20, Byte), Integer), CType(CType(204, Byte), Integer), CType(CType(96, Byte), Integer))
+        Me.btnCultivos.OnHovercolor = System.Drawing.Color.SeaGreen
         Me.btnCultivos.OnHoverTextColor = System.Drawing.Color.White
         Me.btnCultivos.selected = False
         Me.btnCultivos.Size = New System.Drawing.Size(119, 43)
@@ -207,7 +223,7 @@ Partial Class scrMain
         Me.btnTerr.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.btnTerr.Name = "btnTerr"
         Me.btnTerr.Normalcolor = System.Drawing.Color.FromArgb(CType(CType(3, Byte), Integer), CType(CType(109, Byte), Integer), CType(CType(25, Byte), Integer))
-        Me.btnTerr.OnHovercolor = System.Drawing.Color.FromArgb(CType(CType(20, Byte), Integer), CType(CType(204, Byte), Integer), CType(CType(96, Byte), Integer))
+        Me.btnTerr.OnHovercolor = System.Drawing.Color.SeaGreen
         Me.btnTerr.OnHoverTextColor = System.Drawing.Color.White
         Me.btnTerr.selected = False
         Me.btnTerr.Size = New System.Drawing.Size(119, 43)
@@ -251,6 +267,7 @@ Partial Class scrMain
         '
         'btnClose
         '
+        Me.btnClose.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.btnClose.BackColor = System.Drawing.Color.FromArgb(CType(CType(39, Byte), Integer), CType(CType(251, Byte), Integer), CType(CType(107, Byte), Integer))
         Me.btnClose.Cursor = System.Windows.Forms.Cursors.Hand
         Me.btnClose.Image = Global.Agromatic.My.Resources.Resources.Cancel
@@ -265,6 +282,7 @@ Partial Class scrMain
         '
         'btnMax
         '
+        Me.btnMax.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.btnMax.BackColor = System.Drawing.Color.FromArgb(CType(CType(39, Byte), Integer), CType(CType(251, Byte), Integer), CType(CType(107, Byte), Integer))
         Me.btnMax.Cursor = System.Windows.Forms.Cursors.Hand
         Me.btnMax.Image = Global.Agromatic.My.Resources.Resources.Max
@@ -279,6 +297,7 @@ Partial Class scrMain
         '
         'btnMin
         '
+        Me.btnMin.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.btnMin.BackColor = System.Drawing.Color.FromArgb(CType(CType(39, Byte), Integer), CType(CType(251, Byte), Integer), CType(CType(107, Byte), Integer))
         Me.btnMin.Cursor = System.Windows.Forms.Cursors.Hand
         Me.btnMin.Image = Global.Agromatic.My.Resources.Resources.Min
@@ -293,6 +312,8 @@ Partial Class scrMain
         '
         'BunifuSeparator1
         '
+        Me.BunifuSeparator1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.BunifuSeparator1.BackColor = System.Drawing.Color.Transparent
         Me.BunifuSeparator1.ForeColor = System.Drawing.SystemColors.Control
         Me.BunifuSeparator1.LineColor = System.Drawing.Color.FromArgb(CType(CType(39, Byte), Integer), CType(CType(251, Byte), Integer), CType(CType(107, Byte), Integer))
@@ -304,22 +325,6 @@ Partial Class scrMain
         Me.BunifuSeparator1.TabIndex = 1
         Me.BunifuSeparator1.Transparency = 255
         Me.BunifuSeparator1.Vertical = False
-        '
-        'BunifuSeparator2
-        '
-        Me.BunifuSeparator2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.BunifuSeparator2.BackColor = System.Drawing.Color.Transparent
-        Me.BunifuSeparator2.ForeColor = System.Drawing.SystemColors.Control
-        Me.BunifuSeparator2.LineColor = System.Drawing.Color.FromArgb(CType(CType(39, Byte), Integer), CType(CType(251, Byte), Integer), CType(CType(107, Byte), Integer))
-        Me.BunifuSeparator2.LineThickness = 1
-        Me.BunifuSeparator2.Location = New System.Drawing.Point(4, 49)
-        Me.BunifuSeparator2.Margin = New System.Windows.Forms.Padding(4, 6, 4, 6)
-        Me.BunifuSeparator2.Name = "BunifuSeparator2"
-        Me.BunifuSeparator2.Size = New System.Drawing.Size(1020, 25)
-        Me.BunifuSeparator2.TabIndex = 4
-        Me.BunifuSeparator2.Transparency = 255
-        Me.BunifuSeparator2.Vertical = False
         '
         'scrMain
         '
@@ -357,7 +362,7 @@ Partial Class scrMain
     Friend WithEvents pnlWrapp As Panel
     Friend WithEvents btnTerr As Bunifu.Framework.UI.BunifuFlatButton
     Friend WithEvents btnCultivos As Bunifu.Framework.UI.BunifuFlatButton
-    Friend WithEvents btnSens As Bunifu.Framework.UI.BunifuFlatButton
+    Friend WithEvents btnMetro As Bunifu.Framework.UI.BunifuFlatButton
     Friend WithEvents btnRecur As Bunifu.Framework.UI.BunifuFlatButton
     Friend WithEvents BunifuSeparator2 As Bunifu.Framework.UI.BunifuSeparator
 End Class
--- Code/Net/Agromatic/Pantallas/scrMain.vb
+++ Code/Net/Agromatic/Pantallas/scrMain.vb
@@ -1,3 +1,167 @@
 ﻿Public Class scrMain
 
+#Region "VARIABLES"
+
+#End Region
+
+#Region "FUNCIONES"
+
+#End Region
+
+#Region "METODOS"
+
+    ''' <summary>
+    ''' Se encarga de crear un nuevo objeto del tipo Form
+    ''' Busca el formulario recibido como parámetro en la colección
+    ''' Asigna la creación del formulario recibido al objeto creado
+    ''' Le asigna los parámetros necesarios para mostrarlo
+    ''' Lo muestra y lo trae al frente
+    ''' </summary>
+    ''' <typeparam name="MiForm"></typeparam>
+    Private Sub AbrirFormPanel(Of MiForm As {Form, New})()
+
+        'Objeto
+        Dim form As Form
+
+        'Busca el formulario en la colección
+        form = pnlWrapp.Controls.OfType(Of MiForm)().FirstOrDefault()
+
+        'Si el formulario no es encontrado / no existe
+        If form Is Nothing Then
+            form = New MiForm()
+            form.TopLevel = False
+            form.FormBorderStyle = FormBorderStyle.None
+            form.Dock = DockStyle.Fill
+            pnlWrapp.Controls.Add(form)
+            pnlWrapp.Tag = form
+            form.Show()
+            form.BringToFront()
+        Else
+            form.BringToFront()
+        End If
+
+    End Sub
+
+#End Region
+
+#Region "EVENTOS"
+
+    ''' <summary>
+    ''' Cierra el formulario
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Me.Close()
+    End Sub
+
+    ''' <summary>
+    ''' Minimiza el formulario
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnMax_Click(sender As Object, e As EventArgs) Handles btnMax.Click
+
+        'Valida el estado de la pantalla para tomar la acción correspondiente
+        If Me.WindowState = 0 Then
+            'Max
+            Me.WindowState = FormWindowState.Maximized
+        ElseIf Me.WindowState = 2 Then
+            'Min
+            Me.WindowState = FormWindowState.Normal
+        End If
+
+    End Sub
+
+    ''' <summary>
+    ''' Minimiza el formulario
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnMin_Click(sender As Object, e As EventArgs) Handles btnMin.Click
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+
+    ''' <summary>
+    ''' Cambia el color del botón seleccionado
+    ''' Cambia el color de los demás botones
+    ''' Muestra el formulario de Terreno
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnTerr_Click(sender As Object, e As EventArgs) Handles btnTerr.Click
+
+        'Colores
+        btnTerr.Normalcolor = Color.FromArgb(20, 204, 96)
+        btnCultivos.Normalcolor = Color.FromArgb(3, 3, 25)
+        btnMetro.Normalcolor = Color.FromArgb(3, 3, 25)
+        btnRecur.Normalcolor = Color.FromArgb(3, 3, 25)
+
+        'Panel
+        AbrirFormPanel(Of scrTerreno)()
+
+    End Sub
+
+    ''' <summary>
+    ''' Cambia el color del botón seleccionado
+    ''' Cambia el color de los demás botones
+    ''' Muestra el formulario de Cultivo
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnCultivos_Click(sender As Object, e As EventArgs) Handles btnCultivos.Click
+
+        'Colores
+        btnCultivos.Normalcolor = Color.FromArgb(20, 204, 96)
+        btnTerr.Normalcolor = Color.FromArgb(3, 3, 25)
+        btnMetro.Normalcolor = Color.FromArgb(3, 3, 25)
+        btnRecur.Normalcolor = Color.FromArgb(3, 3, 25)
+
+        'Panel
+        AbrirFormPanel(Of ScrCultivos)()
+
+    End Sub
+
+    ''' <summary>
+    ''' Cambia el color del botón seleccionado
+    ''' Cambia el color de los demás botones
+    ''' Muestra el formulario de Metrología
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnMetro_Click(sender As Object, e As EventArgs) Handles btnMetro.Click
+
+        'Colores
+        btnMetro.Normalcolor = Color.FromArgb(20, 204, 96)
+        btnTerr.Normalcolor = Color.FromArgb(3, 3, 25)
+        btnCultivos.Normalcolor = Color.FromArgb(3, 3, 25)
+        btnRecur.Normalcolor = Color.FromArgb(3, 3, 25)
+
+        'Panel
+        AbrirFormPanel(Of ScrMetro)()
+
+    End Sub
+
+    ''' <summary>
+    ''' Cambia el color del botón seleccionado
+    ''' Cambia el color de los demás botones
+    ''' Muestra el formulario de Recursos
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnRecur_Click(sender As Object, e As EventArgs) Handles btnRecur.Click
+
+        'Colores
+        btnRecur.Normalcolor = Color.FromArgb(20, 204, 96)
+        btnTerr.Normalcolor = Color.FromArgb(3, 3, 25)
+        btnCultivos.Normalcolor = Color.FromArgb(3, 3, 25)
+        btnMetro.Normalcolor = Color.FromArgb(3, 3, 25)
+
+        'Panel
+        AbrirFormPanel(Of ScrRecursos)()
+
+    End Sub
+
+#End Region
+
 End Class
